35a9178226e1bcb01350c7c5821fd6af93f71efa
pkozub <pavlo.kozub@gmail.com>
2020-01-06 15:40:41 +0200

    github url fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,12 +8,9 @@ variable "aws-region" {
     default = "eu-west-1"
     description = "Ireland"
 }
-variable "github_personal_token" {
-    default = "TOKEN_HERE"
-    description = "github personal token"  
-}
+variable "github_personal_token" {}
 
 variable "github_url" {
-    default = "https://github.com/perfsys/patients-claims-typescript-sample.git?ref=dev"
+    default = "https://github.com/perfsys/trello-reminder.git?ref=dev"
     description = "github url"
 }
